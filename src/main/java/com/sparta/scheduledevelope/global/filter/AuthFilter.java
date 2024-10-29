0484333041662168b69226ee0e0a3d276afb9211
Dohyeon_Parrk <parkyj3213@naver.com>
2024-10-29 20:26:13 +0900

    refactor : filter 추가 & security passwordencoder 삭제

--- src/main/java/com/sparta/scheduledevelope/global/filter/AuthFilter.java
+++ src/main/java/com/sparta/scheduledevelope/global/filter/AuthFilter.java
@@ -1,102 +1,76 @@
-// package com.sparta.scheduledevelope.global.filter;
-//
-// import com.sparta.scheduledevelope.domain.user.entity.User;
-// import com.sparta.scheduledevelope.domain.user.entity.UserRoleEnum;
-// import com.sparta.scheduledevelope.domain.user.repository.UserRepository;
-// import com.sparta.scheduledevelope.common.util.JwtUtil;
-// import io.jsonwebtoken.Claims;
-// import jakarta.servlet.*;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-// import lombok.extern.slf4j.Slf4j;
-// import org.springframework.core.annotation.Order;
-// import org.springframework.stereotype.Component;
-// import org.springframework.util.StringUtils;
-//
-// import java.io.IOException;
-//
-// @Slf4j(topic = "AuthFilter")
-// @Component
-// @Order(2)
-// public class AuthFilter implements Filter {
-//
-//     private final JwtUtil jwtUtil;
-//     private final UserRepository userRepository;
-//
-//     public AuthFilter(JwtUtil jwtUtil, UserRepository userRepository) {
-//         this.jwtUtil = jwtUtil;
-//         this.userRepository = userRepository;
-//     }
-//
-//     @Override
-//     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-//         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-//
-//         String url = httpServletRequest.getRequestURI();
-//
-//         try {
-//             // 회원가입, 로그인 API는 인증 없이 통과
-//             if (isPublicUrl(url)) {
-//                 chain.doFilter(request, response);
-//                 return;
-//             }
-//
-//             // 헤더에서 JWT 토큰 추출
-//             String tokenValue = jwtUtil.getTokenFromRequest(httpServletRequest);
-//             if (!StringUtils.hasText(tokenValue)) {
-//                 log.error("토큰이 없습니다.");
-//                 httpServletResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "JWT 토큰이 없습니다.");
-//                 return;
-//             }
-//
-//             // JWT 토큰 검증
-//             String token = tokenValue;
-//             if (!jwtUtil.validateToken(token)) {
-//                 log.error("유효하지 않은 JWT 토큰입니다.");
-//                 httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다.");
-//                 return;
-//             }
-//
-//             // 토큰에서 사용자 정보 추출 및 권한 추출
-//             Claims userInfo = jwtUtil.getUserInfoFromToken(token);
-//             User user = (User) userRepository.findByUsername(userInfo.getSubject())
-//                     .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
-//
-//             // 일정 수정 및 삭제 권한 체크 -> ADMIN 관리자만 가능
-//             if (isScheduleHandleAuthorization(httpServletRequest) && user.getRole() != UserRoleEnum.ADMIN) {
-//                 log.error("권한이 없는 사용자입니다.");
-//                 httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "권한이 없습니다.");
-//                 return;
-//             }
-//
-//             // 사용자 정보를 요청 속성에 저장
-//             request.setAttribute("user", user);
-//
-//             // 다음 필터로 전달
-//             chain.doFilter(request, response);
-//
-//         } catch (Exception e) {
-//             log.error("인증 오류: {}", e.getMessage());
-//             httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증 오류가 발생했습니다" + e.getMessage());
-//         }
-//     }
-//
-//     // 공개 API 경로 체크
-//     private boolean isPublicUrl(String url) {
-//         return url.startsWith("/api/user/signup") || url.startsWith("/api/user/login");
-//     }
-//
-//     // 권한 부여 및 일정 수정, 삭제 권한이 필요한 .URL
-//     private boolean isScheduleHandleAuthorization(HttpServletRequest request) {
-//         String url = request.getRequestURI();
-//         String method = request.getMethod();
-//
-//         // 권한 부여 -> PUT /api/admin
-//         // 일정 수정 -> PUT /api/schedule/{id}
-//         // 일정 삭제 -> DELETE /api/schedule/{id}
-//         return (url.startsWith("/api/admin") && url.startsWith("/api/schedule/") && (method.equals("PUT") || method.equals("DELETE")));
-//     }
-// }
-//
-//
+package com.sparta.scheduledevelope.global.filter;
+
+import java.io.IOException;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import com.sparta.scheduledevelope.common.auth.JwtUtil;
+import com.sparta.scheduledevelope.domain.user.entity.Member;
+import com.sparta.scheduledevelope.domain.user.repository.MemberRepository;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "AuthFilter")
+@Component
+@Order(2)
+public class AuthFilter implements Filter {
+
+	private final MemberRepository memberRepository;
+	private final JwtUtil jwtUtil;
+
+	public AuthFilter(MemberRepository memberRepository, JwtUtil jwtUtil) {
+		this.memberRepository = memberRepository;
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		String url = httpServletRequest.getRequestURI();
+
+		if (StringUtils.hasText(url) &&
+			(url.startsWith("/api/auth"))
+		) {
+			// 회원가입, 로그인 관련 API 는 인증 필요없이 요청 진행
+			chain.doFilter(request, response); // 다음 Filter 로 이동
+		} else {
+			// 나머지 API 요청은 인증 처리 진행
+			// 토큰 확인
+			String tokenValue = jwtUtil.getTokenFromRequest(httpServletRequest);
+
+			if (StringUtils.hasText(tokenValue)) { // 토큰이 존재하면 검증 시작
+				// JWT 토큰 substring
+				String token = jwtUtil.substringToken(tokenValue);
+
+				// 토큰 검증
+				if (!jwtUtil.validateToken(token)) {
+					throw new IllegalArgumentException("Token Error");
+				}
+
+				// 토큰에서 사용자 정보 가져오기
+				Claims info = jwtUtil.getUserInfoFromToken(token);
+
+				Member member = memberRepository.findByEmail(info.getSubject()).orElseThrow(() ->
+					new NullPointerException("Not Found Member")
+				);
+
+				request.setAttribute("member", member);
+				chain.doFilter(request, response); // 다음 Filter 로 이동
+			} else {
+				throw new IllegalArgumentException("Not Found Token");
+			}
+		}
+	}
+
+}
+
+
